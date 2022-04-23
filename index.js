8a38b5badbcda9371a9c24c5d36e8bf831a1469a
estevesryan <ryanplay71@gmail.com>
2022-04-23 13:30:31 -0300

    consertando bugs

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ function clock() {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-
-    if (seconds < 10) {
-        seconds = "0" + seconds
+    if (hour < 10) {
+        hour = "0" + hour;
     }
+
     if (minutes < 10) {
-        minutes = "o" + minutes
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
     }
 
-    const hourBlock = document.getElementById('hour')
+
+    const hourBlock = document.getElementById('hour');
     const minutesBlock = document.getElementById('minutes');
     const secondsBlock = document.getElementById('seconds');
 
-    hourBlock.innerHTML = hour
-    minutesBlock.innerHTML = minutes
-    secondsBlock.innerHTML = seconds
+    hourBlock.innerHTML = hour;
+    minutesBlock.innerHTML = minutes;
+    secondsBlock.innerHTML = seconds;
 }
 timer = setInterval(clock, 1000);
